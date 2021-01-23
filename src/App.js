69ce9a37ad9a8d4507847d63060df584f387d917
kanekiken1234 <ruthvikrokz@gmail.com>
2021-01-23 20:51:12 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from "./components/Navbar";
+import Search from './pages/Search';
+import Lyrics from './pages/Lyrics';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route path='/lyrics' exact component={Lyrics} />
+        <Route path='/' exact component={Search} />
+      </Switch>
+
     </div>
   );
 }
